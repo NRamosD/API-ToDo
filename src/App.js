678c8c8d2afecd9d7d12adfbb8bae1288761e212
NRamosD <nixonramosdue@gmail.com>
2021-01-28 17:59:33 -0500

    Proyecto subido a github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useEffect , useState} from 'react';
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos/'
+  const [todos, setTodos] = useState()
+  const fetchApi = async () =>{
+    const response = await fetch(url)
+    console.log(response.statusText)
+    console.log(response.status)
+    const responseJSON = await response.json()
+    setTodos(responseJSON)
+    console.log(responseJSON)
+  }
+  useEffect(()=>{
+    fetchApi()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      DATOS DE LA API TODOS
+      {!todos ? "Cargando..." : 
+      todos.map((todo, index) =>{
+        //mapeo de los todos
+        return <li>{todo.title} {todo.completed ? '👍':'❌'}</li>
+      })}
     </div>
   );
 }
